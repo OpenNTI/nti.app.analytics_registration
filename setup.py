00633209a572022b4bbe377db90a412951657571
Josh Zuech <josh.zuech@nextthought.com>
2016-03-23 20:02:53 +0000

    Initial checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.analytics_registration/trunk@85167 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,36 @@
+import codecs
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+
+entry_points = {
+    "z3c.autoinclude.plugin": [
+		'target = nti.app',
+	],
+}
+
+setup(
+	name='nti.app.analytics_registration',
+	version=VERSION,
+	author='Josh Zuech',
+	author_email='josh.zuech@nextthought.com',
+	description="NTI App Analytics Registration",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='pyramid preference',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+	],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti'],
+	install_requires=[
+		'setuptools',
+		'nti.analytics_registration'
+	],
+	entry_points=entry_points
+)
