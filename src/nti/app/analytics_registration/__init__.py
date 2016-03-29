3cc2da5f12cedd30aa92f20cd5f28f78773781a2
Josh Zuech <josh.zuech@nextthought.com>
2016-03-29 18:59:22 +0000

    Add admin views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.analytics_registration/trunk@85485 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/analytics_registration/__init__.py
+++ src/nti/app/analytics_registration/__init__.py
@@ -12,6 +12,17 @@ logger = __import__('logging').getLogger(__name__)
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory(__name__)
 
+#: The registration path part.
 REGISTRATION = 'registration'
+
+#: A view to return available sessions for enrollment.
+REGISTRATION_AVAILABLE_SESSIONS = 'RegistrationAvailableSessions'
+
+#: A POST view to submit registration/survey.
 SUBMIT_REGISTRATION_INFO = 'SubmitRegistration'
+
+#: A view to return enrollment rules for registration data.
+REGISTRATION_ENROLL_RULES = 'RegistrationEnrollRules'
+
+#: The admin view to fetch registration csv.
 REGISTRATION_READ_VIEW = 'Registrations'
--- src/nti/app/analytics_registration/views.py
+++ src/nti/app/analytics_registration/views.py
@@ -9,21 +9,9 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import csv
-import nameparser
-
-from io import BytesIO
-
 from nti.app.analytics_registration import MessageFactory as _
 
-from zope import interface
-from zope import component
-
 from zope.event import notify
-
-from zope.container.contained import Contained
-
-from zope.traversing.interfaces import IPathAdapter
 
 from pyramid.view import view_config
 
@@ -36,7 +24,6 @@ from nti.app.analytics.utils import set_research_status
 
 from nti.app.analytics_registration.interfaces import UserRegistrationSurveySubmissionEvent
 
-from nti.analytics_registration.registration import get_registrations
 from nti.analytics_registration.registration import store_registration_data
 from nti.analytics_registration.registration import store_registration_survey_data
 
@@ -46,17 +33,10 @@ from nti.common.maps import CaseInsensitiveDict
 from nti.dataserver import authorization as nauth
 
 from nti.dataserver.interfaces import IUser
-from nti.dataserver.interfaces import IUsernameSubstitutionPolicy
-
-from nti.dataserver.users import User
-
-from nti.dataserver.users.interfaces import IUserProfile
 
 from nti.externalization.interfaces import StandardExternalFields
 
-from nti.app.analytics_registration import REGISTRATION
 from nti.app.analytics_registration import SUBMIT_REGISTRATION_INFO
-from nti.app.analytics_registration import REGISTRATION_READ_VIEW
 
 CLASS = StandardExternalFields.CLASS
 LAST_MODIFIED = StandardExternalFields.LAST_MODIFIED
@@ -64,23 +44,6 @@ LAST_MODIFIED = StandardExternalFields.LAST_MODIFIED
 def _is_true(t):
 	result = bool(t and str(t).lower() in TRUE_VALUES)
 	return result
-
-def replace_username(username):
-	substituter = component.queryUtility(IUsernameSubstitutionPolicy)
-	if substituter is None:
-		return username
-	result = substituter.replace(username) or username
-	return result
-
-@interface.implementer(IPathAdapter)
-class RegistrationPathAdapter(Contained):
-
-	__name__ = REGISTRATION
-
-	def __init__(self, context, request):
-		self.context = context
-		self.request = request
-		self.__parent__ = context
 
 @view_config(route_name='objects.generic.traversal',
 			 name=SUBMIT_REGISTRATION_INFO,
@@ -129,69 +92,3 @@ class RegistrationPostView(AbstractAuthenticatedView,
 		notify( UserRegistrationSurveySubmissionEvent( user, data ))
 		return hexc.HTTPNoContent()
 
-@view_config(route_name='objects.generic.traversal',
-			 renderer='rest',
-			 permission=nauth.ACT_NTI_ADMIN,
-			 context=RegistrationPathAdapter,
-			 request_method='GET',
-			 name=REGISTRATION_READ_VIEW)
-class RegistrationCSVView(AbstractAuthenticatedView):
-	"""
-	An admin view to fetch all registration data.
-	"""
-
-	def _get_names_and_email(self, user, username):
-		profile = IUserProfile( user )
-		external_id = replace_username(username)
-
-		realname = profile.realname or ''
-		if realname and '@' not in realname and realname != username:
-			human_name = nameparser.HumanName(realname)
-			firstname = human_name.first or ''
-			lastname = human_name.last or ''
-		else:
-			firstname = ''
-			lastname = ''
-		email = getattr( profile, 'email', '' )
-		return external_id, firstname, lastname, email
-
-	def __call__(self):
-		values = CaseInsensitiveDict( self.readInput() )
-		username = values.get( 'user' ) or values.get( 'username' )
-		registration_id = values.get( 'registration_id' ) or values.get( 'RegistrationId' )
-
-		user = User.get_user( username )
-		# Optionally filter by user or registration id.
-		registrations = get_registrations( user=user, registration_id=registration_id )
-		if not registrations:
-			return hexc.HTTPNotFound( _('There are no registrations') )
-
-		stream = BytesIO()
-		csv_writer = csv.writer( stream )
-		header_row = [u'username', u'first_name', u'last_name', u'email', u'phone',
-					  u'school', u'grade', u'session_range', u'curriculum']
-		csv_writer.writerow( header_row )
-
-		for registration in registrations:
-			username = registration.user.username
-			user = User.get_user( username )
-			if user is None:
-				logger.warn( 'User not found (%s)', username )
-				continue
-			username, first, last, email = self._get_names_and_email( user, username )
-			line_data = (username,
-						 first,
-						 last,
-						 email,
-						 registration.phone,
-						 registration.school,
-						 registration.grade_teaching,
-						 registration.session_range,
-						 registration.curriculum)
-			csv_writer.writerow( line_data )
-
-		response = self.request.response
-		response.body = stream.getvalue()
-		response.content_type = str('text/csv; charset=UTF-8')
-		response.content_disposition = b'attachment; filename="registrations.csv"'
-		return response
