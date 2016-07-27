579c6df1ab77630cedaa8c9b86424fadc8af30db
Josh Zuech <josh.zuech@nextthought.com>
2016-07-27 15:40:11 +0000

    Refactor our registration/survey logic to mixin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.analytics_registration/trunk@93177 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/analytics_registration/admin_views.py
+++ src/nti/app/analytics_registration/admin_views.py
@@ -10,7 +10,6 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import csv
-import nameparser
 
 from nti.app.analytics_registration import MessageFactory as _
 
@@ -19,7 +18,6 @@ from collections import namedtuple
 from io import BytesIO
 
 from zope import interface
-from zope import component
 
 from zope.container.contained import Contained
 
@@ -29,7 +27,9 @@ from pyramid.view import view_config
 
 from pyramid import httpexceptions as hexc
 
+from nti.app.analytics_registration.view_mixins import RegistrationCSVMixin
 from nti.app.analytics_registration.view_mixins import RegistrationIDViewMixin
+from nti.app.analytics_registration.view_mixins import RegistrationSurveyCSVMixin
 
 from nti.app.base.abstract_views import get_source
 from nti.app.base.abstract_views import AbstractAuthenticatedView
@@ -37,7 +37,6 @@ from nti.app.base.abstract_views import AbstractAuthenticatedView
 from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
 
 from nti.analytics_registration.registration import get_user_registrations
-from nti.analytics_registration.registration import get_all_survey_questions
 from nti.analytics_registration.registration import store_registration_rules
 from nti.analytics_registration.registration import delete_user_registrations
 from nti.analytics_registration.registration import store_registration_sessions
@@ -51,11 +50,7 @@ from nti.contenttypes.courses.interfaces import ICourseEnrollmentManager
 
 from nti.dataserver import authorization as nauth
 
-from nti.dataserver.interfaces import IUsernameSubstitutionPolicy
-
 from nti.dataserver.users import User
-
-from nti.dataserver.users.interfaces import IUserProfile
 
 from nti.externalization.interfaces import StandardExternalFields
 
@@ -70,13 +65,6 @@ from nti.app.analytics_registration import REGISTRATION_AVAILABLE_SESSIONS
 
 CLASS = StandardExternalFields.CLASS
 LAST_MODIFIED = StandardExternalFields.LAST_MODIFIED
-
-def replace_username(username):
-	substituter = component.queryUtility(IUsernameSubstitutionPolicy)
-	if substituter is None:
-		return username
-	result = substituter.replace(username) or username
-	return result
 
 @interface.implementer(IPathAdapter)
 class RegistrationPathAdapter(Contained):
@@ -94,57 +82,18 @@ class RegistrationPathAdapter(Contained):
 			 context=RegistrationPathAdapter,
 			 request_method='GET',
 			 name=REGISTRATION_READ_VIEW)
-class RegistrationCSVView(AbstractAuthenticatedView,
-						  RegistrationIDViewMixin):
+class RegistrationCSVView( AbstractAuthenticatedView,
+						   RegistrationCSVMixin,
+						   RegistrationIDViewMixin):
 	"""
 	An admin view to fetch all registration data.
 	"""
 
-	def _get_names_and_email(self, user, username):
-		profile = IUserProfile( user )
-		external_id = replace_username(username)
-
-		realname = profile.realname or ''
-		if realname and '@' not in realname and realname != username:
-			human_name = nameparser.HumanName(realname)
-			firstname = human_name.first or ''
-			lastname = human_name.last or ''
-		else:
-			firstname = ''
-			lastname = ''
-		email = getattr( profile, 'email', '' )
-		return external_id, firstname, lastname, email
-
 	def _get_header_row(self):
-		header_row = [u'username', u'first_name', u'last_name', 'registration_date',
-					  u'employee_id', u'email', u'phone',
-					  u'school', u'grade', u'session_range', u'curriculum']
-		return header_row
+		return self._get_registration_header_row()
 
 	def _get_row_data(self, registration):
-		if not registration.user:
-			logger.warn( 'User no longer exists' )
-			return
-		username = registration.user.username
-		user = User.get_user( username )
-		if user is None:
-			logger.warn( 'User not found (%s)', username )
-			return
-		username, first, last, email = self._get_names_and_email( user, username )
-		if email and email.endswith( '@nextthought.com' ):
-			return
-		line_data = {'username': username,
-					 'first_name': first,
-					 'last_name': last,
-					 'registration_date': registration.timestamp,
-					 'employee_id': registration.employee_id,
-					 'email': email,
-					 'phone': registration.phone,
-					 'school': registration.school,
-					 'grade': registration.grade_teaching,
-					 'session_range': registration.session_range,
-					 'curriculum': registration.curriculum}
-		return line_data
+		return self._get_registration_row_data( registration )
 
 	def __call__(self):
 		values = CaseInsensitiveDict( self.request.params )
@@ -181,55 +130,28 @@ class RegistrationCSVView(AbstractAuthenticatedView,
 			 context=RegistrationPathAdapter,
 			 request_method='GET',
 			 name=REGISTRATION_SURVEY_READ_VIEW)
-class RegistrationSurveyCSVView( RegistrationCSVView ):
+class RegistrationSurveyCSVView( RegistrationCSVView,
+								 RegistrationSurveyCSVMixin ):
 	"""
 	An admin view to fetch all registration data and survey data in
 	a CSV.
 	"""
 
-	def _get_question_key(self, question_id):
-		# Remove whitespace
-		return '_'.join( question_id.split() )
-
-	def _get_survey_display(self, question_id):
-		return 'Survey: %s' % question_id
-
-	@Lazy
-	def _survey_question_map(self):
-		"""
-		Map survey question identifier to display version.
-		"""
-		registration_id = self._get_registration_id()
-		survey_questions = get_all_survey_questions( registration_id )
-		survey_question_map = {self._get_question_key( x ): self._get_survey_display( x )
-							   for x in survey_questions }
-		return survey_question_map
+	def _get_header_row(self):
+		result = []
+		result.extend( self._get_registration_header_row() )
+		result.extend( self._get_registration_survey_header_row() )
+		return result
 
 	def _get_row_data(self, registration):
-		line_data = super( RegistrationSurveyCSVView, self )._get_row_data( registration )
-		if not line_data:
+		result = {}
+		registration_data = self._get_registration_row_data( registration )
+		if not registration_data:
 			return None
-		survey_submission = registration.survey_submission[0]
-		line_data['survey_version'] = survey_submission.survey_version
-
-		# Gather our user responses
-		user_results = {}
-		for submission in survey_submission.details:
-			key = self._get_question_key( submission.question_id )
-			user_results[ key ] = submission.response
-
-		# Now map to our result set, making sure to provide empty string
-		# for no-responses.
-		for key, display in self._survey_question_map.items():
-			line_data[display] = user_results.get( key, '' )
-		return line_data
-
-	def _get_header_row(self):
-		header_row = super( RegistrationSurveyCSVView, self )._get_header_row()
-		header_row.append( 'survey_version' )
-		questions = sorted( self._survey_question_map.values() )
-		header_row.extend( questions )
-		return header_row
+		result.update( registration_data )
+		survey_data = self._get_registration_survey_row_data( registration )
+		result.update( survey_data )
+		return result
 
 @view_config(route_name='objects.generic.traversal',
 			 renderer='rest',
--- src/nti/app/analytics_registration/view_mixins.py
+++ src/nti/app/analytics_registration/view_mixins.py
@@ -9,11 +9,32 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import nameparser
+
+from zope import component
+
+from nti.analytics_registration.registration import get_all_survey_questions
+
 from nti.app.analytics_registration import MessageFactory as _
 
 from pyramid import httpexceptions as hexc
 
 from nti.common.maps import CaseInsensitiveDict
+
+from nti.common.property import Lazy
+
+from nti.dataserver.interfaces import IUsernameSubstitutionPolicy
+
+from nti.dataserver.users import User
+
+from nti.dataserver.users.interfaces import IUserProfile
+
+def replace_username(username):
+	substituter = component.queryUtility(IUsernameSubstitutionPolicy)
+	if substituter is None:
+		return username
+	result = substituter.replace(username) or username
+	return result
 
 class RegistrationIDViewMixin(object):
 	"""
@@ -41,3 +62,94 @@ class RegistrationIDViewMixin(object):
 			raise hexc.HTTPUnprocessableEntity( _('No registration id given.') )
 		return result
 
+	def _get_names_and_email(self, user, username):
+		profile = IUserProfile( user )
+		external_id = replace_username(username)
+
+		realname = profile.realname or ''
+		if realname and '@' not in realname and realname != username:
+			human_name = nameparser.HumanName(realname)
+			firstname = human_name.first or ''
+			lastname = human_name.last or ''
+		else:
+			firstname = ''
+			lastname = ''
+		email = getattr( profile, 'email', '' )
+		return external_id, firstname, lastname, email
+
+	def _get_registration_row_data(self, registration):
+		if not registration.user:
+			logger.warn( 'User no longer exists' )
+			return
+		username = registration.user.username
+		user = User.get_user( username )
+		if user is None:
+			logger.warn( 'User not found (%s)', username )
+			return
+		username, first, last, email = self._get_names_and_email( user, username )
+		if email and email.endswith( '@nextthought.com' ):
+			return
+		line_data = {'username': username,
+					 'first_name': first,
+					 'last_name': last,
+					 'registration_date': registration.timestamp,
+					 'employee_id': registration.employee_id,
+					 'email': email,
+					 'phone': registration.phone,
+					 'school': registration.school,
+					 'grade': registration.grade_teaching,
+					 'session_range': registration.session_range,
+					 'curriculum': registration.curriculum}
+		return line_data
+
+class RegistrationCSVMixin( object ):
+
+	def _get_registration_header_row(self):
+		header_row = [u'username', u'first_name', u'last_name', 'registration_date',
+					  u'employee_id', u'email', u'phone',
+					  u'school', u'grade', u'session_range', u'curriculum']
+		return header_row
+
+class RegistrationSurveyCSVMixin(RegistrationCSVMixin):
+
+	def _get_question_key(self, question_id):
+		# Remove whitespace
+		return '_'.join( question_id.split() )
+
+	def _get_survey_display(self, question_id):
+		return 'Survey: %s' % question_id
+
+	@Lazy
+	def _survey_question_map(self):
+		"""
+		Map survey question identifier to display version.
+		"""
+		registration_id = self._get_registration_id()
+		survey_questions = get_all_survey_questions( registration_id )
+		survey_question_map = {self._get_question_key( x ): self._get_survey_display( x )
+							   for x in survey_questions}
+		return survey_question_map
+
+	def _get_registration_survey_header_row(self):
+		header_row = []
+		header_row.append( 'survey_version' )
+		questions = sorted( self._survey_question_map.values() )
+		header_row.extend( questions )
+		return header_row
+
+	def _get_registration_survey_row_data(self, registration):
+		line_data = {}
+		survey_submission = registration.survey_submission[0]
+		line_data['survey_version'] = survey_submission.survey_version
+
+		# Gather our user responses
+		user_results = {}
+		for submission in survey_submission.details:
+			key = self._get_question_key( submission.question_id )
+			user_results[ key ] = submission.response
+
+		# Now map to our result set, making sure to provide empty string
+		# for no-responses.
+		for key, display in self._survey_question_map.items():
+			line_data[display] = user_results.get( key, '' )
+		return line_data
